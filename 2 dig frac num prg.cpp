8015c195e2b5202b252b157f493ca56eef9c57de
madhumitha-debug <madhumithaboobalan@gmail.com>
2025-09-01 12:01:09 +0530

    Add files via upload

---
+++ 2 dig frac num prg.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	float a;
+	printf("enter a number");
+	scanf("%f",&a);
+	printf("The 2 digit fractional number %f",a);
+	return 0;
+}
---
+++ fractional num prg.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+	float num=1.123;
+	printf("the fractional number %f",num);
+	return 0;
+}
